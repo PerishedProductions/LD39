e3b5e46a38b4b37ec25c4634546b346468a016ca
Mikkel Mouridsen <mikkelmouridsen@me.com>
2017-07-29 07:00:10 +0200

    Init

---
+++ LD39/GameStates/GameState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD39.GameStates
+{
+    public class GameState
+    {
+
+        GameState(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public Game1 game;
+
+        public virtual void Init()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+
+    }
+}
