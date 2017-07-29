2797fdddb5f0ac3148e93a9f06a633691b03d752
Mikkel Mouridsen <mikkelmouridsen@me.com>
2017-07-29 09:03:26 +0200

    Added mockup

--- LD39/Game1.cs
+++ LD39/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using LD39.GameStates;
 using System.Collections.Generic;
+using System;
 
 namespace LD39
 {
--- LD39/GameStates/GameState.cs
+++ LD39/GameStates/GameState.cs
@@ -14,11 +14,15 @@ namespace LD39.GameStates
         public Game1 game;
         public Camera cam;
 
+        private SpriteFont fnt;
+
         public virtual void Init()
         {
             cam = new Camera(game.getGraphics());
             cam.LoadContent(game.GraphicsDevice);
             cam.Debug.IsVisible = true;
+
+            fnt = game.Content.Load<SpriteFont>("Fonts/fnt");
         }
 
         public virtual void Update(GameTime gameTime)
@@ -29,7 +33,7 @@ namespace LD39.GameStates
         public virtual void Draw(SpriteBatch batch)
         {
             batch.Begin(cam);
-            
+            batch.DrawString(fnt, "Test :D", Vector2.Zero, Color.White);
             batch.End();
         }
 
