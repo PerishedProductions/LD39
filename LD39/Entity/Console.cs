e395453807e2289bfa0a4a3386a39941577f1000
Dragion <Dragion147@hotmail.com>
2017-07-29 15:53:24 +0200

    Ability to Write, but only on the final row!

--- LD39/Entity/Console.cs
+++ LD39/Entity/Console.cs
@@ -26,6 +26,7 @@ namespace LD39.Entity
 
         public int ConsoleTopLogLine { get; set; } = 0;
         public int ConsoleLogMaxVisibleLines { get; set; } = 20;
+        public int ConsoleMaxLines { get; set; } = 100;
 
         public Console(Vector2 position, Texture2D texture, SpriteFont font) : base(position, texture)
         {
@@ -62,6 +63,7 @@ namespace LD39.Entity
             ConsoleLog.Add("Computer3");
             ConsoleLog.Add("Power3");
             ConsoleLog.Add("Ludum Dare3");
+            ConsoleLog.Add(" ");
         }
 
         public override void Update(GameTime gameTime)
@@ -75,6 +77,7 @@ namespace LD39.Entity
             }
 
             UpdateCursorPosition();
+            UpdateInput();
         }
 
         private void UpdateCursorPosition()
@@ -181,6 +184,85 @@ namespace LD39.Entity
 
             CursorPosition = CursorPosition + new Vector2(1, 0);
             return true;
+        }
+
+        private void UpdateInput()
+        {
+            if (input.isPressed(Keys.Q)) WriteLetter("Q");
+            if (input.isPressed(Keys.W)) WriteLetter("W");
+            if (input.isPressed(Keys.E)) WriteLetter("E");
+            if (input.isPressed(Keys.R)) WriteLetter("R");
+            if (input.isPressed(Keys.T)) WriteLetter("T");
+            if (input.isPressed(Keys.Y)) WriteLetter("Y");
+            if (input.isPressed(Keys.U)) WriteLetter("U");
+            if (input.isPressed(Keys.I)) WriteLetter("I");
+            if (input.isPressed(Keys.O)) WriteLetter("O");
+            if (input.isPressed(Keys.P)) WriteLetter("P");
+            if (input.isPressed(Keys.A)) WriteLetter("A");
+            if (input.isPressed(Keys.S)) WriteLetter("S");
+            if (input.isPressed(Keys.D)) WriteLetter("D");
+            if (input.isPressed(Keys.F)) WriteLetter("F");
+            if (input.isPressed(Keys.G)) WriteLetter("G");
+            if (input.isPressed(Keys.H)) WriteLetter("H");
+            if (input.isPressed(Keys.J)) WriteLetter("J");
+            if (input.isPressed(Keys.K)) WriteLetter("K");
+            if (input.isPressed(Keys.L)) WriteLetter("L");
+            if (input.isPressed(Keys.Z)) WriteLetter("Z");
+            if (input.isPressed(Keys.X)) WriteLetter("X");
+            if (input.isPressed(Keys.C)) WriteLetter("C");
+            if (input.isPressed(Keys.V)) WriteLetter("V");
+            if (input.isPressed(Keys.B)) WriteLetter("B");
+            if (input.isPressed(Keys.N)) WriteLetter("N");
+            if (input.isPressed(Keys.M)) WriteLetter("M");
+
+            if (input.isPressed(Keys.D0) || input.isPressed(Keys.NumPad0)) WriteLetter("0");
+            if (input.isPressed(Keys.D1) || input.isPressed(Keys.NumPad1)) WriteLetter("1");
+            if (input.isPressed(Keys.D2) || input.isPressed(Keys.NumPad2)) WriteLetter("2");
+            if (input.isPressed(Keys.D3) || input.isPressed(Keys.NumPad3)) WriteLetter("3");
+            if (input.isPressed(Keys.D4) || input.isPressed(Keys.NumPad4)) WriteLetter("4");
+            if (input.isPressed(Keys.D5) || input.isPressed(Keys.NumPad5)) WriteLetter("5");
+            if (input.isPressed(Keys.D6) || input.isPressed(Keys.NumPad6)) WriteLetter("6");
+            if (input.isPressed(Keys.D7) || input.isPressed(Keys.NumPad7)) WriteLetter("7");
+            if (input.isPressed(Keys.D8) || input.isPressed(Keys.NumPad8)) WriteLetter("8");
+            if (input.isPressed(Keys.D9) || input.isPressed(Keys.NumPad9)) WriteLetter("9");
+
+            if (input.isPressed(Keys.OemPeriod)) WriteLetter(".");
+            if (input.isPressed(Keys.OemPlus)) WriteLetter("+");
+            if (input.isPressed(Keys.OemMinus)) WriteLetter("-");
+            if (input.isPressed(Keys.OemComma)) WriteLetter(",");
+            if (input.isPressed(Keys.Space)) WriteLetter(" ");
+            if (input.isPressed(Keys.Divide)) WriteLetter("/");
+            if (input.isPressed(Keys.Multiply)) WriteLetter("*");
+            if (input.isPressed(Keys.OemBackslash)) WriteLetter("\\");
+
+
+            if (input.isPressed(Keys.Delete))
+            {
+
+            }
+
+            if (input.isPressed(Keys.Enter))
+            {
+
+            }
+
+            if (input.isPressed(Keys.Back))
+            {
+
+            }
+        }
+
+        private void WriteLetter(string letter)
+        {
+            if (CursorPosition.Y == ConsoleTopLogLine + ConsoleLogMaxVisibleLines - 1)
+            {
+                string line = ConsoleLog[ConsoleLog.Count - 1];
+
+                line = line.Insert((int)CursorPosition.X, letter);
+                ConsoleLog[ConsoleLog.Count - 1] = line;
+
+                MoveCursorRight();
+            }
         }
 
         public override void Draw(SpriteBatch batch)
