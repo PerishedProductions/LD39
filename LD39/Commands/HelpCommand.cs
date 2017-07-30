9a0ce8f158f21dca61d175863d58535d483da46a
Dragion <Dragion147@hotmail.com>
2017-07-30 20:09:30 +0200

    Help command should be more helpfull

--- LD39/Commands/HelpCommand.cs
+++ LD39/Commands/HelpCommand.cs
@@ -12,33 +12,57 @@ namespace LD39.Commands
 
         public override void DisplayHelp()
         {
-            feedback.Add("The help command displays some helpfull information...");
+            feedback.Add("The help command displays some helpful information...");
 
-            PerformCommandWithoutArguments();
+            commandAction(feedback);
         }
 
         public override void DisplayUsage()
         {
             feedback.Add("Usage: help");
-            feedback.Add("No arguments, no nothing.");
+            feedback.Add("Even though the help states of a test argument. There is no test argument available for use");
 
-            PerformCommandWithoutArguments();
+            commandAction(feedback);
         }
 
         public override bool HasRequiredArguments(Dictionary<string, string> arguments)
         {
+            if (arguments.ContainsKey("-test"))
+            {
+                return true;
+            }
+
             return false;
         }
 
         public override void PerformCommandWithArguments(Dictionary<string, string> arguments)
         {
-            //Should never be called but just in case
-            PerformCommandWithoutArguments();
+            string testValue = arguments["-test"];
+
+            if (string.IsNullOrEmpty(testValue))
+            {
+                feedback.Add("You called help with the -test argument succesfully. In this case -test has no value assigned.");
+            }
+            else
+            {
+                feedback.Add($"You called help with the -test argument succesfully. In this case -test has a value assigned. Value='{testValue}'");
+            }
+
+            commandAction(feedback);
         }
 
         public override void PerformCommandWithoutArguments()
         {
-            feedback.Add("TODO: Insert basic help here.");
+            feedback.Add("This game is heavily focused around using the console.");
+            feedback.Add("There are several commands available to perform your hacking deeds.");
+            feedback.Add("Some can be used without any arguments, other may need them or even require them.");
+            feedback.Add("It is usefull to know that all commands have a -h for a small description.");
+            feedback.Add("Example: help -h");
+            feedback.Add("There is also a standard -u for usage explanation for each command.");
+            feedback.Add("Example: help -u");
+            feedback.Add("In some situations arguments require values. These may be provided by typing in the argument followed by an '=' and the value");
+            feedback.Add("Example: help -test=value");
+            feedback.Add("To get yourself started. Try the examples and otherwise use the 'commands' command to retrieve a list of available commands");
             commandAction(feedback);
         }
     }
